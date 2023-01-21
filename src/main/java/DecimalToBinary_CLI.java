a68716057f84ff8a90807458c80e90f478e55b81
Greg Sabella <106336554+gsabella4@users.noreply.github.com>
2023-01-21 09:15:28 -0500

    pushing updates

--- src/main/java/DecimalToBinary_CLI.java
+++ src/main/java/DecimalToBinary_CLI.java
@@ -11,46 +11,62 @@ public class DecimalToBinary_CLI {
             binaryResult = String.valueOf(eachModulus) + binaryResult;
             binaryInteger /= 2;
         }
+        int bitCount = Integer.bitCount(input);
         System.out.println("\n" + input + " converted to binary is " + binaryResult);
+        System.out.println("The bitcount is " + bitCount);
+    }
+
+    public static void binaryToDecimal(String binaryInput){
+        int decimal = Integer.parseInt(binaryInput, 2);
+        System.out.println("\n" + binaryInput + " converted to decimal is " + decimal);
     }
 
     public static void main(String[] args) {
 
-        System.out.println("\n\n\t Welecome to the Decimal to Binary Converter!");
-        System.out.println("\n\t ...and a ASCII Art Gameboy. Why not?");
-        System.out.println("\n_n_________________");
-        System.out.println("|_|_______________|_|");
-        System.out.println("|  ,-------------.  |");
-        System.out.println("| |  .---------.  | |");
-        System.out.println("| |  |         |  | |");
-        System.out.println("| |  |         |  | |");
-        System.out.println("| |  |         |  | |");
-        System.out.println("| |  |         |  | |");
-        System.out.println("| |  `---------'  | |");
-        System.out.println("| `---------------' |");
-        System.out.println("|   _ GAME BOY      |");
-        System.out.println("| _| |_         ,-. |");
-        System.out.println("||_ O _|   ,-. \"._,\"|");
-        System.out.println("|  |_|    \"._,\"   A |");
-        System.out.println("|    _  _    B      |");
-        System.out.println("|   // //           |");
-        System.out.println("|  // //    \\\\\\\\\\\\  |");
-        System.out.println("|  `  `      \\\\\\\\\\\\ ,");
-        System.out.println("|________...______,\"");
+        // To-do:
+        // - exception handling
+        // - input, output classes
+        // - classes to hold methods
+        // - tweak formatting
+
+        System.out.println("\n\n\t Welcome to the Binary Converter!");
+        System.out.println("\n☆彡･:*:･★彡･:*:･☆彡･:*:･★彡･:*:･☆彡･:*:･★彡");
 
         Scanner scanner = new Scanner(System.in);
         int status = 1;
         while(status == 1){
-        System.out.print("\nEnter a number to be converted into Binary: ");
-        int input = Integer.parseInt(scanner.nextLine());
-        decimalToBinary(input);
-
-            System.out.print("\nWould you like to run another conversion? Y/N   ");
-        if(scanner.nextLine().equalsIgnoreCase("Y")){
-            continue;
+        System.out.println("\n****************************************");
+        System.out.println("*                                      *");
+        System.out.println("*          --- Main Menu ---           *");
+            System.out.println("*                                      *");
+            System.out.println("****************************************\n");
+        System.out.println("   Decimal to Binary? ---> Press 1");
+        System.out.println("   Binary to Decimal? ---> Press 2");
+        System.out.print("\n   Selection: ");
+        String mainSelection = scanner.nextLine();
+        if(mainSelection.equals("1")) {
+            System.out.print("\nWhat would you like to convert to Binary? Enter here ---> ");
+            int input = Integer.parseInt(scanner.nextLine());
+            decimalToBinary(input);
+        } else if (mainSelection.equals("2")){
+            System.out.print("\nWhat Binary value would you like to convert? Enter here --->  ");
+            String binaryInput = scanner.nextLine();
+            binaryToDecimal(binaryInput);
         } else {
-            System.out.println("\n----------------- Goodbye! -----------------");
-            status++;
+            System.out.println("\nDidn't recognize that input! Back to main");
+            continue;
+        }
+
+        System.out.print("\nWould you like to run another conversion? Y/N   ");
+        String anotherRound = scanner.nextLine();
+            if(anotherRound.equalsIgnoreCase("Y")){
+                continue;
+            } else if (anotherRound.equalsIgnoreCase("N")){
+                System.out.println("\n----------------- Goodbye! -----------------");
+                status++;
+            } else {
+                System.out.println("\nDidn't recognize that input! Back to main");
+                continue;
             }
         }
     }
